fd3e88dd6c4cbb505bfabdfabc04429f660240a8
Timo Glastra <timo@glastra.me>
2020-01-12 18:57:08 +0100

    build: added script to automate license headers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 ula-aca.
+ * Copyright 2019-present ula-aca
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
